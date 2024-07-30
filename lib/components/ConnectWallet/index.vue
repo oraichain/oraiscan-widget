b2867bbebc4db76d821226b4493bce08ba568718
ledanghuy1811 <huygamo567@gmail.com>
2024-07-30 17:15:34 +0700

    feat: add owallet

--- lib/components/ConnectWallet/index.vue
+++ lib/components/ConnectWallet/index.vue
@@ -23,8 +23,12 @@ const sending = ref(false);
 const open = ref(false);
 const error = ref('');
 
-const name = ref(WalletName.Keplr);
+const name = ref(WalletName.Owallet);
 const list = [
+    {
+        wallet: WalletName.Owallet,
+        logo: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6d/cf/9c/6dcf9c8f-45d9-37a4-d5c3-0da8d4af0650/AppIcon-0-0-1x_U007emarketing-0-6-0-85-220.png/460x0w.webp',
+    },
     {
         wallet: WalletName.Keplr,
         logo: 'https://ping.pub/logos/keplr-logo.svg',
@@ -40,21 +44,22 @@ const list = [
     {
         wallet: WalletName.Metamask,
         logo: 'https://ping.pub/logos/metamask.png',
-    }
+    },
 ];
 
-async function initData() { }
+async function initData() {}
 
 const walletList = computed(() => {
     // const l = list
-    return list
-})
+    return list;
+});
 
 const connected = ref(readWallet(props.hdPath) as ConnectedWallet);
 
 function selectWallet(wallet: WalletName) {
     name.value = wallet;
 }
+
 async function connect() {
     sending.value = true;
     error.value = '';
@@ -91,6 +96,7 @@ async function connect() {
     }
     sending.value = false;
 }
+
 function disconnect() {
     removeWallet(props.hdPath);
     emit('disconnect', { value: connected.value });
@@ -98,8 +104,8 @@ function disconnect() {
 }
 
 function keplr() {
-    emit('keplr-config', {})
-    open.value = false
+    emit('keplr-config', {});
+    open.value = false;
 }
 
 let showCopyToast = ref(0);
@@ -124,20 +130,31 @@ const tipMsg = computed(() => {
         : { class: 'success', msg: 'Copy Success!' };
 });
 const customKeplr = computed(() => {
-    return `${location.protocol}//${location.host}/wallet/keplr?chain=${props.chainId}`
-})
+    return `${location.protocol}//${location.host}/wallet/keplr?chain=${props.chainId}`;
+});
 </script>
+
 <template>
     <div class="mb-4">
         <!-- modal btn -->
-        <div v-if="connected.cosmosAddress" class="dropdown dropdown-hover ping-connect-dropdown">
-            <label tabindex="0" class="btn btn-sm m-1 lowercase">{{ connected.wallet }}-{{
-                connected.cosmosAddress?.substring(
-                    connected.cosmosAddress?.length - 4
-                )
-            }}</label>
-            <div tabindex="0" class="dropdown-content menu shadow p-2 bg-base-100 rounded w-64 overflow-auto">
-                <div class="px-2 mb-1 text-gray-500 dark:text-gray-400 font-semibold flex justify-between">
+        <div
+            v-if="connected.cosmosAddress"
+            class="dropdown dropdown-hover ping-connect-dropdown"
+        >
+            <label tabindex="0" class="btn btn-sm m-1 lowercase"
+                >{{ connected.wallet }}-{{
+                    connected.cosmosAddress?.substring(
+                        connected.cosmosAddress?.length - 4
+                    )
+                }}</label
+            >
+            <div
+                tabindex="0"
+                class="dropdown-content menu shadow p-2 bg-base-100 rounded w-64 overflow-auto"
+            >
+                <div
+                    class="px-2 mb-1 text-gray-500 dark:text-gray-400 font-semibold flex justify-between"
+                >
                     <span class="text-lg"> {{ connected.wallet }} </span>
                     <span class="ml-2 text-xs mt-2">
                         {{ connected.hdPath }}
@@ -145,8 +162,11 @@ const customKeplr = computed(() => {
                 </div>
                 <div class="">
                     <div class="divider mt-1 mb-1"></div>
-                    <a class="block py-2 px-2 hover:bg-gray-100 dark:hover:bg-[#353f5a] rounded cursor-pointer"
-                        style="overflow-wrap: anywhere" @click="copyAdress(connected.cosmosAddress)">
+                    <a
+                        class="block py-2 px-2 hover:bg-gray-100 dark:hover:bg-[#353f5a] rounded cursor-pointer"
+                        style="overflow-wrap: anywhere"
+                        @click="copyAdress(connected.cosmosAddress)"
+                    >
                         {{ connected.cosmosAddress }}
                     </a>
                     <!-- <div class="divider mt-1 mb-1"></div>
@@ -160,8 +180,11 @@ const customKeplr = computed(() => {
                         to="/wallet/portfolio"
                         >Portfolio</RouterLink
                     > -->
-                    <a class="block py-2 px-2 hover:bg-gray-100 dark:hover:bg-[#353f5a] rounded cursor-pointer"
-                        @click="disconnect()">Disconnect</a>
+                    <a
+                        class="block py-2 px-2 hover:bg-gray-100 dark:hover:bg-[#353f5a] rounded cursor-pointer"
+                        @click="disconnect()"
+                        >Disconnect</a
+                    >
                 </div>
             </div>
             <div class="toast" v-show="showCopyToast === 1">
@@ -179,25 +202,54 @@ const customKeplr = computed(() => {
                 </div>
             </div>
         </div>
-        <label v-if="!connected.cosmosAddress" for="PingConnectWallet"
-            class="btn btn-sm ping-connect-btn capitalize">Connect Wallet</label>
+        <label
+            v-if="!connected.cosmosAddress"
+            for="PingConnectWallet"
+            class="btn btn-sm ping-connect-btn capitalize"
+            >Connect Wallet</label
+        >
 
         <!-- modal content -->
-        <input v-model="open" type="checkbox" id="PingConnectWallet" class="modal-toggle" @change="initData()" />
+        <input
+            v-model="open"
+            type="checkbox"
+            id="PingConnectWallet"
+            class="modal-toggle"
+            @change="initData()"
+        />
 
         <label for="PingConnectWallet" class="modal cursor-pointer z-[999999]">
             <label class="modal-box rounded-lg" for="">
                 <h3 class="text-xl font-semibold">Connect Wallet</h3>
-                <ul role="list" class="bg-gray-100 dark:bg-gray-900 rounded-lg mt-4 px-3 py-3">
-                    <li class="flex items-center px-2 py-3 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-lg cursor-pointer"
-                        v-for="(i, k) of walletList" :key="k" @click="selectWallet(i.wallet)">
-                        <img class="h-10 w-10 bg-gray-50 rounded-full mr-4" :src="i.logo" alt="" />
-                        <p class="text-base font-semibold flex-1 dark:text-gray-300">
+                <ul
+                    role="list"
+                    class="bg-gray-100 dark:bg-gray-900 rounded-lg mt-4 px-3 py-3"
+                >
+                    <li
+                        class="flex items-center px-2 py-3 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-lg cursor-pointer"
+                        v-for="(i, k) of walletList"
+                        :key="k"
+                        @click="selectWallet(i.wallet)"
+                    >
+                        <img
+                            class="h-10 w-10 bg-gray-50 rounded-full mr-4"
+                            :src="i.logo"
+                            alt=""
+                        />
+                        <p
+                            class="text-base font-semibold flex-1 dark:text-gray-300"
+                        >
                             {{ i.wallet }}
                         </p>
                         <div>
-                            <div v-if="i.wallet === name" class="mr-4 rounded-full bg-green-200">
-                                <Icon icon="mdi:check" class="font-bold text-green-600" />
+                            <div
+                                v-if="i.wallet === name"
+                                class="mr-4 rounded-full bg-green-200"
+                            >
+                                <Icon
+                                    icon="mdi:check"
+                                    class="font-bold text-green-600"
+                                />
                             </div>
                         </div>
                     </li>
@@ -205,15 +257,23 @@ const customKeplr = computed(() => {
                 <div v-show="error" class="text-error mt-3">
                     <span>{{ error }}.</span>
                     <div v-show="String(error).search('no chain info') > -1">
-                        <a :href="customKeplr" class="btn btn-link">Suggest a chain to Keplr</a>
+                        <a :href="customKeplr" class="btn btn-link"
+                            >Suggest a chain to Keplr</a
+                        >
                     </div>
                 </div>
                 <div class="mt-8 text-right flex">
                     <label class="btn mr-1" @click="keplr">
                         <Icon icon="mdi:cog-outline" />
                     </label>
-                    <label class="btn btn-primary ping-connect-confirm grow" @click="connect()">
-                        <span v-if="sending" class="loading loading-spinner"></span>
+                    <label
+                        class="btn btn-primary ping-connect-confirm grow"
+                        @click="connect()"
+                    >
+                        <span
+                            v-if="sending"
+                            class="loading loading-spinner"
+                        ></span>
                         Connect
                     </label>
                 </div>
